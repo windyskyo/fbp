181d75ec9ffd44aca6c6a08fc75c3d49fa531484
windyskyo <133635419+windyskyo@users.noreply.github.com>
2023-12-18 14:27:33 +0900

    세팅

---
+++ fbp/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}', // 모든 페이지와 컴포넌트 포함
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
