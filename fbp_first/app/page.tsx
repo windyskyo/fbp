e1cb594e2bced701d472e995acaaee4b3a36f6bb
windyskyo <133635419+windyskyo@users.noreply.github.com>
2023-12-20 14:42:23 +0900

    ㅇㅇ

--- fbp_first/app/page.tsx
+++ fbp_first/app/page.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import Image from 'next/image';
 import React, { useEffect } from 'react';
 
@@ -8,26 +10,6 @@ declare global {
 }
 
 export default function Home() {
-  useEffect(() => {
-    const kakaoMapScript = document.createElement('script');
-    kakaoMapScript.async = false;
-    kakaoMapScript.src = `//dapi.kakao.com/v2/maps/sdk.js?appkey=700d399006256f95732f06b19c046ba5&autoload=false`;
-    document.head.appendChild(kakaoMapScript);
-
-    const onLoadKakaoAPI = () => {
-      window.kakao.maps.load(() => {
-        var container = document.getElementById('map');
-        var options = {
-          center: new window.kakao.maps.LatLng(33.450701, 126.570667),
-          level: 3,
-        };
-
-        var map = new window.kakao.maps.Map(container, options);
-      });
-    };
-
-    kakaoMapScript.addEventListener('load', onLoadKakaoAPI);
-  }, []);
   return (
     <div className='flex justify-center py-20 text-gray-500 bg-white'>
       <div className='w-full max-w-sm p-4'>
@@ -38,7 +20,7 @@ export default function Home() {
           <p className='mt-3 text-right'>2024년 1월 20일 토요일 오후 3시</p>
           <div className='py-6'>
             <Image
-              src='/img/img_baby_1.jpg'
+              src='/img/img_baby_main.jpg'
               alt='은호 사진'
               className=''
               width={3000}
@@ -95,7 +77,7 @@ export default function Home() {
             <h3 className='pb-12'>GALLERY</h3>
             <div className='space-y-4'>
               <Image
-                src='/img/img_baby_1.jpg'
+                src='/img/img_baby_2.jpg'
                 alt='은호 사진'
                 className=''
                 width={3000}
@@ -103,7 +85,7 @@ export default function Home() {
                 priority
               />
               <Image
-                src='/img/img_baby_1.jpg'
+                src='/img/img_baby_3.jpg'
                 alt='은호 사진'
                 className=''
                 width={3000}
@@ -111,7 +93,7 @@ export default function Home() {
                 priority
               />
               <Image
-                src='/img/img_baby_1.jpg'
+                src='/img/img_baby_4.jpg'
                 alt='은호 사진'
                 className=''
                 width={3000}
@@ -119,7 +101,7 @@ export default function Home() {
                 priority
               />
               <Image
-                src='/img/img_baby_1.jpg'
+                src='/img/img_baby_5.jpg'
                 alt='은호 사진'
                 className=''
                 width={3000}
@@ -127,7 +109,7 @@ export default function Home() {
                 priority
               />
               <Image
-                src='/img/img_baby_1.jpg'
+                src='/img/img_baby_6.jpg'
                 alt='은호 사진'
                 className=''
                 width={3000}
@@ -148,8 +130,130 @@ export default function Home() {
           </div>
           <div className='text-center leading-8'>
             <h3 className='pb-12'>LOCATION</h3>
-            <div className='w-full h-[50vh] sm:h-[60vh] md:h-[70vh] lg:h-[80vh]'>
-              <div id='map' style={{ width: '100%', height: '100%' }}></div>
+            <div
+              style={{
+                fontFamily: 'normal normal 400 12px/normal dotum, sans-serif',
+                width: '360px',
+                height: '512px',
+                color: '#333',
+                position: 'relative',
+              }}
+            >
+              <div style={{ height: '480px' }}>
+                <a
+                  href='https://map.kakao.com/?urlX=447716.0&urlY=1165222.0&itemId=1491938807&q=더블유페스타 일산점&srcid=1491938807&map_type=TYPE_MAP&from=roughmap'
+                  target='_blank'
+                >
+                  <img
+                    className='map'
+                    src='http://t1.daumcdn.net/roughmap/imgmap/847a8590e48e31ccf33f929c7eafd937dd4497b6a5b1d9cc07a947a1f59d1825'
+                    width='358px'
+                    height='478px'
+                    style={{ border: '1px solid #ccc' }}
+                    alt='Map'
+                  />
+                </a>
+              </div>
+              <div
+                style={{
+                  overflow: 'hidden',
+                  padding: '7px 11px',
+                  border: '1px solid rgba(0, 0, 0, 0.1)',
+                  borderRadius: '0px 0px 2px 2px',
+                  backgroundColor: 'rgb(249, 249, 249)',
+                }}
+              >
+                <a
+                  href='https://map.kakao.com'
+                  target='_blank'
+                  style={{ float: 'left' }}
+                >
+                  <img
+                    src='//t1.daumcdn.net/localimg/localimages/07/2018/pc/common/logo_kakaomap.png'
+                    width='72'
+                    height='16'
+                    alt='카카오맵'
+                    style={{ display: 'block', width: '72px', height: '16px' }}
+                  />
+                </a>
+                <div
+                  style={{
+                    float: 'right',
+                    position: 'relative',
+                    top: '1px',
+                    fontSize: '11px',
+                  }}
+                >
+                  <a
+                    target='_blank'
+                    href='https://map.kakao.com/?from=roughmap&srcid=1491938807&confirmid=1491938807&q=더블유페스타 일산점&rv=on'
+                    style={{
+                      float: 'left',
+                      height: '15px',
+                      paddingTop: '1px',
+                      lineHeight: '15px',
+                      color: '#000',
+                      textDecoration: 'none',
+                    }}
+                  >
+                    로드뷰
+                  </a>
+                  <span
+                    style={{
+                      width: '1px',
+                      padding: '0',
+                      margin: '0 8px 0 9px',
+                      height: '11px',
+                      verticalAlign: 'top',
+                      position: 'relative',
+                      top: '2px',
+                      borderLeft: '1px solid #d0d0d0',
+                      float: 'left',
+                    }}
+                  ></span>
+                  <a
+                    target='_blank'
+                    href='https://map.kakao.com/?from=roughmap&eName=더블유페스타 일산점&eX=447716.0&eY=1165222.0'
+                    style={{
+                      float: 'left',
+                      height: '15px',
+                      paddingTop: '1px',
+                      lineHeight: '15px',
+                      color: '#000',
+                      textDecoration: 'none',
+                    }}
+                  >
+                    길찾기
+                  </a>
+                  <span
+                    style={{
+                      width: '1px',
+                      padding: '0',
+                      margin: '0 8px 0 9px',
+                      height: '11px',
+                      verticalAlign: 'top',
+                      position: 'relative',
+                      top: '2px',
+                      borderLeft: '1px solid #d0d0d0',
+                      float: 'left',
+                    }}
+                  ></span>
+                  <a
+                    target='_blank'
+                    href='https://map.kakao.com?map_type=TYPE_MAP&from=roughmap&srcid=1491938807&itemId=1491938807&q=더블유페스타 일산점&urlX=447716.0&urlY=1165222.0'
+                    style={{
+                      float: 'left',
+                      height: '15px',
+                      paddingTop: '1px',
+                      lineHeight: '15px',
+                      color: '#000',
+                      textDecoration: 'none',
+                    }}
+                  >
+                    지도 크게 보기
+                  </a>
+                </div>
+              </div>
             </div>
           </div>
         </div>
